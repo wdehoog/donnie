4a00dda379870daca90a3bb12cacb186033c28b2
Вячеслав Диконов <sdiconov@mail.ru>
2018-06-21 15:00:54 +0300

    Russian translation

---
+++ translations/donnie-ru.ts
@@ -0,0 +1,478 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>UPnP control point and player for Sailfish OS</source>
+        <translation>Контроллер и проигрыватель UPnP для ОС Sailfish</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Переводы</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Благодарность</translation>
+    </message>
+    <message>
+        <source>Copyright (C) 2017 Willem-Jan de Hoog</source>
+        <extracomment>I doubt this needs to be translated</extracomment>
+        <translation>Автор (C) 2017 Willem-Jan de Hoog</translation>
+    </message>
+</context>
+<context>
+    <name>Browse</name>
+    <message>
+        <source>Browse</source>
+        <translation>Содержимое сервера</translation>
+    </message>
+    <message>
+        <source>Load Previous Set</source>
+        <translation>Предыдущий набор</translation>
+    </message>
+    <message>
+        <source>Load Next Set</source>
+        <translation>Следующий набор</translation>
+    </message>
+    <message>
+        <source>Load More</source>
+        <translation>Загрузить ещё</translation>
+    </message>
+    <message>
+        <source>Add To Player</source>
+        <translation>Добавить в список</translation>
+    </message>
+    <message>
+        <source>Replace In Player</source>
+        <translation>Заменить в списке</translation>
+    </message>
+    <message>
+        <source>Add All To Player</source>
+        <translation>Добавить всё</translation>
+    </message>
+    <message>
+        <source>Replace All In Player</source>
+        <translation>Заменить всё</translation>
+    </message>
+    <message>
+        <source>Choose Path</source>
+        <translation>Выбрать путь</translation>
+    </message>
+    <message>
+        <source>[Top Level]</source>
+        <translation>[Корень]</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Donnie</source>
+        <translation>Donnie</translation>
+    </message>
+</context>
+<context>
+    <name>DiscoveryPage</name>
+    <message>
+        <source>Discover UPnP Devices</source>
+        <translation>Поиск UPnP-устройств</translation>
+    </message>
+    <message>
+        <source>UPnP Devices</source>
+        <translation>Устройства</translation>
+    </message>
+    <message>
+        <source>No Devices</source>
+        <translation>Нет устройств</translation>
+    </message>
+    <message>
+        <source>Built-in Player</source>
+        <translation>Встроенный проигрыватель</translation>
+    </message>
+    <message>
+        <source>Sailfish QTAudio Player</source>
+        <translation>Sailfish QTAudio Player</translation>
+    </message>
+    <message>
+        <source>a page with audio player controls and list of tracks</source>
+        <translation>страница управления воспроизведением</translation>
+    </message>
+    <message>
+        <source>Renderer</source>
+        <translation>Проигрыватель</translation>
+    </message>
+    <message>
+        <source>Content Server</source>
+        <translation>Сервер данных</translation>
+    </message>
+</context>
+<context>
+    <name>EditURIDialog</name>
+    <message>
+        <source>Stream Type</source>
+        <translation>Тип потока</translation>
+    </message>
+    <message>
+        <source>Type of stream the URI referes to</source>
+        <translation>Формат потока указанного URI</translation>
+    </message>
+    <message>
+        <source>Music Track</source>
+        <translation>Музыкальная дорожка</translation>
+    </message>
+    <message>
+        <source>Audio Broadcast</source>
+        <translation>Звуковое вещание</translation>
+    </message>
+    <message>
+        <source>URI</source>
+        <translation>URI</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+</context>
+<context>
+    <name>LogPage</name>
+    <message>
+        <source>Log</source>
+        <translation>Журнал</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройка</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
+        <source>Donnie</source>
+        <translation>Donnie</translation>
+    </message>
+    <message>
+        <source>Renderer</source>
+        <translation>Проигрыватель</translation>
+    </message>
+    <message>
+        <source>[Click to select one]</source>
+        <translation>[устройство не задано]</translation>
+    </message>
+    <message>
+        <source>Content Server</source>
+        <translation>Сервер медиаданных</translation>
+    </message>
+    <message>
+        <source>Browser</source>
+        <translation>Просмотр</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation>Проигрыватель</translation>
+    </message>
+    <message>
+        <source>Failed to load previously saved Browse Stack Ids.</source>
+        <translation>Невозможно загрузить ранее сохранённые Browse Stack Id</translation>
+    </message>
+    <message>
+        <source>Failed to load previously saved Queue Ids.
+Can not Resume.</source>
+        <translation>Невозможно загрузить ранее сохранённые Queue Id.
+Нельзя продолжить.</translation>
+    </message>
+    <message>
+        <source>There seems to be no internet connection (wifi off). Your Renderer and Media Server might not be reachable.</source>
+        <translation>Нет связи с Интернетом (выключен wifi). Ваши устройства воспроизведения и источники данных могут быть недоступны.</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
+        <source>Load previously saved queue?</source>
+        <translation>Загрузить сохранённую очередь</translation>
+    </message>
+    <message>
+        <source>Load</source>
+        <translation>Загрузка</translation>
+    </message>
+    <message>
+        <source>Failed to retrieve metadata for previously saved Ids.
+Can not Resume.</source>
+        <translation>Невозможно загрузить метаданные для ранее сохранённых Queue Id.
+Нельзя продолжить.</translation>
+    </message>
+    <message>
+        <source>[Top Level]</source>
+        <translation>[Корень]</translation>
+    </message>
+    <message>
+        <source>Failed to parse previously saved Ids.
+Can not Resume.</source>
+        <translation>Невозможно использовать ранее сохранённые Id.
+Нельзя продолжить.</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <source>Empty List</source>
+        <translation>Очистить список</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Add Stream</source>
+        <translation>Добавить поток</translation>
+    </message>
+</context>
+<context>
+    <name>RendererPage</name>
+    <message>
+        <source>Failed to Pause the Renderer</source>
+        <translation>Не удалось поставить дорожку на паузу</translation>
+    </message>
+    <message>
+        <source>Failed to Start the Renderer</source>
+        <translation>Не удалось остановить проигрыватель</translation>
+    </message>
+    <message>
+        <source>Failed to Stop the Renderer</source>
+        <translation>Не удалось остановить проигрыватель</translation>
+    </message>
+    <message>
+        <source>Failed to set volume on Renderer</source>
+        <translation>Не удалось изменить громкость проигрывателя</translation>
+    </message>
+    <message>
+        <source>Failed to mute/unmute Renderer</source>
+        <translation>Не удалось включить/выключить звук у проигрывателя</translation>
+    </message>
+    <message>
+        <source>Empty List</source>
+        <translation>Очистить список</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Failed to set track to play on Renderer:</source>
+        <translation>Не удалось задать дорожку для воспроизведения:</translation>
+    </message>
+    <message>
+        <source>Failed to set track to play on Renderer</source>
+        <translation>Не удалось задать дорожку для воспроизведения</translation>
+    </message>
+    <message>
+        <source>Failed to set next track to play on Renderer:</source>
+        <translation>Не удалось задать следующую дорожку для воспроизведения:</translation>
+    </message>
+    <message>
+        <source>Failed to set next track to play on Renderer</source>
+        <translation>Не удалось задать следующую дорожку для воспроизведения</translation>
+    </message>
+    <message>
+        <source>The renderer has stopped playing unexpectedly.</source>
+        <translation>Проигрыватель неожиданно остановился</translation>
+    </message>
+    <message>
+        <source>Lost connection with Renderer.</source>
+        <translation>Соединеие с проигрывателем разорвано</translation>
+    </message>
+    <message>
+        <source>Add Stream</source>
+        <translation>Добавить поток</translation>
+    </message>
+</context>
+<context>
+    <name>Search</name>
+    <message>
+        <source>Search</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <source>Load Previous Set</source>
+        <translation>Загрузить предыдущий набор</translation>
+    </message>
+    <message>
+        <source>Load Next Set</source>
+        <translation>Загрузить следующий набор</translation>
+    </message>
+    <message>
+        <source>Load More</source>
+        <translation>Загрузить ещё</translation>
+    </message>
+    <message>
+        <source>Search text</source>
+        <translation>Введите запрос</translation>
+    </message>
+    <message>
+        <source>Search In</source>
+        <translation>Искать в</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ничего</translation>
+    </message>
+    <message>
+        <source>Group By</source>
+        <translation>Группировать по</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <source>Add To Player</source>
+        <translation>Добавить в список</translation>
+    </message>
+    <message>
+        <source>Add Group To Player</source>
+        <translation>Добавить группу</translation>
+    </message>
+    <message>
+        <source>Add All To Player</source>
+        <translation>Добавить всё</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройка</translation>
+    </message>
+    <message>
+        <source>How long to search for UPnp Devices (sec)</source>
+        <translation>Сколько ждать ответа UPnP устройств (сек.)</translation>
+    </message>
+    <message>
+        <source>Maximum number of results per request</source>
+        <translation>Предельное число результатов по одному запросу</translation>
+    </message>
+    <message>
+        <source>Allow Containers</source>
+        <translation>Разрешать контейнеры</translation>
+    </message>
+    <message>
+        <source>Also show Containers in search results</source>
+        <translation>Показывать контейнеры в результатах поиска</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Продолжать</translation>
+    </message>
+    <message>
+        <source>Load saved track queue at startup and resume playing</source>
+        <translation>Загружать список медиа и продолжать воспроизведение при запуске</translation>
+    </message>
+    <message>
+        <source>Never</source>
+        <translation>никогда</translation>
+    </message>
+    <message>
+        <source>Ask</source>
+        <translation>по запросу</translation>
+    </message>
+    <message>
+        <source>Always</source>
+        <translation>всегда</translation>
+    </message>
+    <message>
+        <source>Log Page</source>
+        <translation>Журналирование</translation>
+    </message>
+    <message>
+        <source>Show Open Log Page button</source>
+        <translation>Показывать кнопку просмотра журнала</translation>
+    </message>
+</context>
+<context>
+    <name>UPnP</name>
+    <message>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <source>Genre</source>
+        <translation>Жанр</translation>
+    </message>
+    <message>
+        <source>Creator</source>
+        <translation>Автор</translation>
+    </message>
+    <message>
+        <source>Publisher</source>
+        <translation>Издатель</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Описание</translation>
+    </message>
+    <message>
+        <source>User Annotation</source>
+        <translation>Заметки пользователя</translation>
+    </message>
+    <message>
+        <source>Long Description</source>
+        <translation>Полное описание</translation>
+    </message>
+    <message>
+        <source>User entered URI</source>
+        <translation>Пользовательский URI</translation>
+    </message>
+</context>
+<context>
+    <name>UPnPDeviceDetails</name>
+    <message>
+        <source>Device Details</source>
+        <translation>Об устройстве</translation>
+    </message>
+    <message>
+        <source>model: </source>
+        <translation>модель:</translation>
+    </message>
+    <message>
+        <source>manufacturer: </source>
+        <translation>производитель:</translation>
+    </message>
+    <message>
+        <source>Device Type: </source>
+        <translation>Тип устройства:</translation>
+    </message>
+</context>
+</TS>
